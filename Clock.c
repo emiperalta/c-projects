aa5c74c6ccbcd306c85f4db6d82f8d8b1cec63c8
EmiPeralta <emilianop96@hotmail.com>
2021-03-07 13:06:30 -0300

    digital clock created

--- Clock.c
+++ Clock.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
+#include <windows.h>
 
 int main()
 {
-    printf("Hello, world!\n");
+    int hours, minutes, seconds;
+    int delay = 1000;
+
+    printf("Set time: \n");
+
+    printf("Hours: ");
+    scanf("%d", &hours);
+
+    printf("Minutes: ");
+    scanf("%d", &minutes);
+
+    printf("Seconds: ");
+    scanf("%d", &seconds);
+
+    if (hours > 23 || minutes > 59 || seconds > 59)
+    {
+        printf("ERROR!\n");
+        exit(0);
+    }
+    
+    while(1) //infinite loop
+    {
+        seconds++;
+        if (seconds > 59) 
+        {
+            minutes++;
+            seconds = 0;
+        }
+        if (minutes > 59) 
+        {
+            hours++;
+            minutes = 0;
+        }
+        if (hours > 23) 
+        {
+            hours = 0;
+        }
+
+        printf("\n  --------------------------------  ");
+        printf("\n  |        %02d:%02d:%02d              | ", hours, minutes, seconds);
+        printf("\n  --------------------------------  ");
+
+        Sleep(delay);
+        system("cls"); //to clear the screen
+    }
 
     return 0;
 }
